5d43ce38cb76585350a00808a81770dce3552834
Benoit Deziel <benoitdeziel@gmail.com>
2022-12-26 16:08:35 -0500

    chore: update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,24 +2,21 @@ const path = require('path');
 
 module.exports = {
   stories: [],
-  addons: [
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        backgrounds: false,
-      },
-    },
-    'storybook-dark-mode',
-    'storybook-addon-performance/register',
-    '@nrwl/react/plugins/storybook',
-  ],
+  addons: [{
+    name: '@storybook/addon-essentials',
+    options: {
+      backgrounds: false
+    }
+  }, 'storybook-dark-mode', 'storybook-addon-performance/register', '@nrwl/react/plugins/storybook'],
   typescript: {
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
       tsconfigPath: path.resolve(__dirname, './tsconfig.json'),
       shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) =>
-        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
-    },
+      propFilter: prop => prop.parent ? !/node_modules/.test(prop.parent.fileName) : true
+    }
   },
+  core: {
+    builder: 'webpack5'
+  }
 };
