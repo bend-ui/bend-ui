9d642abfb4a0552bce6b84f8eee85b23d6499234
Benoit Deziel <benoitdeziel@gmail.com>
2023-02-12 09:48:07 -0500

    feat(primitives): update tailwind config

--- packages/primitives/tailwind.config.js
+++ packages/primitives/tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 
 const { join } = require('path');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: [join(__dirname, './src/**/*.{js,ts,jsx,tsx}')],
   darkMode: ['class', '.dark-mode'],
   theme: {
     extend: {
-      textColor: {
-        primary: 'var(--color-text-primary)',
-      },
-      backgroundColor: {
-        primary: 'var(--color-bg-primary)',
+      colors: {
+        primary: colors.indigo[500],
+        'primary-emphasis': colors.indigo[800],
+        secondary: colors.yellow[500],
+        neutral: colors.gray[500],
+        canvas: colors.slate[300],
+        surface: colors.slate[600],
       },
     },
   },
