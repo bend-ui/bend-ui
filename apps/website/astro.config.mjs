2dca27e091b264470722d0e3d979e4c3c53ebbad
Benoit Deziel <benoitdeziel@gmail.com>
2023-04-15 08:32:14 -0400

    feat(website): add support for mdx

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   outDir: '../../dist/apps/website',
-  integrations: [react(), sitemap(), tailwind()],
+  integrations: [react(), mdx(), sitemap(), tailwind()],
 });
