9a773e5b79e0664e146efe4dbad86cd259450cab
Benoit Deziel <benoitdeziel@gmail.com>
2023-10-29 11:46:26 -0400

    feat(panda-radix): update Checkbox to use styled

--- packages/panda-radix/src/components/Checkbox/Checkbox.tsx
+++ packages/panda-radix/src/components/Checkbox/Checkbox.tsx
@@ -2,19 +2,32 @@ import { forwardRef } from 'react';
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { TbCheck } from 'react-icons/tb';
 import { cx } from '@particles/panda-system/css';
+import { styled } from '@particles/panda-system/jsx';
 import type { ComponentPropsWithoutRef, ElementRef } from 'react';
 
-const Checkbox = forwardRef<
+export interface CheckboxProps
+  extends ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> {
+  children?: never;
+}
+
+const Root = forwardRef<
   ElementRef<typeof CheckboxPrimitive.Root>,
-  ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <CheckboxPrimitive.Root ref={ref} className={cx(className)} {...props}>
-    <CheckboxPrimitive.Indicator className={cx()}>
-      <TbCheck />
-    </CheckboxPrimitive.Indicator>
-  </CheckboxPrimitive.Root>
-));
+  CheckboxProps
+>((props, ref) => {
+  const { children, className, ...rest } = props;
+  return (
+    <CheckboxPrimitive.Root
+      ref={ref}
+      className={cx('peer', className)}
+      {...rest}
+    >
+      <CheckboxPrimitive.Indicator className={cx()}>
+        <TbCheck />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  );
+});
 
-Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+Root.displayName = CheckboxPrimitive.Root.displayName;
 
-export { Checkbox };
+export const Checkbox = styled(Root);
