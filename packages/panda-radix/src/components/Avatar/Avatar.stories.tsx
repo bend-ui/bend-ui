daedc96eea54e23631318f60788ec56b3fa510d3
Benoit Deziel <benoitdeziel@gmail.com>
2023-10-29 11:43:49 -0400

    feat(panda-radix): add avatar

---
+++ packages/panda-radix/src/components/Avatar/Avatar.stories.tsx
@@ -0,0 +1,15 @@
+import { Avatar } from '.';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default { component: Avatar } as Meta<typeof Avatar>;
+
+type Story = StoryObj<typeof Avatar>;
+
+export const Base: Story = {
+  render: (args) => (
+    <Avatar {...args}>
+      <Avatar.Image alt="@nanopx" src="https://github.com/nanopx.png" />
+      <Avatar.Fallback>NP</Avatar.Fallback>
+    </Avatar>
+  ),
+};
---
+++ packages/panda-radix/src/components/Avatar/Avatar.tsx
@@ -0,0 +1,15 @@
+import * as AvatarPrimitive from '@radix-ui/react-avatar';
+import { styled } from '@particles/panda-system/jsx';
+import { avatar } from '@particles/panda-system/recipes';
+import { createComponent } from '@particles/primitives';
+import { createStyleContext } from '../../utils';
+
+const { withProvider, withContext } = createStyleContext(avatar);
+
+const Root = withProvider(styled(AvatarPrimitive.Root), 'root');
+
+const Image = withContext(styled(AvatarPrimitive.Image), 'image');
+
+const Fallback = withContext(styled(AvatarPrimitive.Fallback), 'fallback');
+
+export const Avatar = createComponent(Root, { Root, Image, Fallback });
