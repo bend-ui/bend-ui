145712d7621738f3b815f56a199edb5b0ce43ca2
Benoit Deziel <benoitdeziel@gmail.com>
2024-02-03 13:19:36 -0500

    feat(react): use Divider from Panda

--- packages/react/src/components/Divider/Divider.tsx
+++ packages/react/src/components/Divider/Divider.tsx
@@ -1,28 +1,26 @@
-import { forwardRef } from '@particles/primitives';
-import { cx } from '@particles/styled-system/css';
-import { divider } from '@particles/styled-system/recipes';
-import { Children, type ComponentPropsWithoutRef, type ReactNode } from 'react';
-import type { DividerVariantProps } from '@particles/styled-system/recipes';
+import { Divider } from '@particles/styled-system/jsx';
 
-export interface DividerProps extends ComponentPropsWithoutRef<'span'> {
-  children?: ReactNode;
-  orientation?: DividerVariantProps['orientation'];
-}
+// export interface DividerProps extends ComponentPropsWithoutRef<'span'> {
+//   children?: ReactNode;
+//   orientation?: DividerVariantProps['orientation'];
+// }
 
-const Root = forwardRef<'span', DividerProps>((props, ref) => {
-  const { children, className, orientation, ...rest } = props;
-  const withContent = Children.count(children) > 0;
-  const recipe = divider({ withContent, orientation });
-  return (
-    <span
-      ref={ref}
-      className={cx(recipe.root, className)}
-      role="separator"
-      {...rest}
-    >
-      {withContent && <span className={recipe.content}>{children}</span>}
-    </span>
-  );
-});
+// const Root = forwardRef<'span', DividerProps>((props, ref) => {
+//   const { children, className, orientation, ...rest } = props;
+//   const withContent = Children.count(children) > 0;
+//   const recipe = divider({ withContent, orientation });
+//   return (
+//     <span
+//       ref={ref}
+//       className={cx(recipe.root, className)}
+//       role="separator"
+//       {...rest}
+//     >
+//       {withContent && <span className={recipe.content}>{children}</span>}
+//     </span>
+//   );
+// });
 
-export const Divider = Object.assign(Root, {});
+// export const Divider = Object.assign(Root, {});
+
+export { Divider };
