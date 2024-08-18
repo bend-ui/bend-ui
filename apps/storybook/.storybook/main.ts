aa0f7c567b2dae32e98d311b2f1aea39f781e9a2
Benoit Deziel <benoitdeziel@gmail.com>
2024-08-18 11:30:35 -0400

    chore: update storybook config

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -10,7 +10,8 @@ const config: StorybookConfig = {
     getAbsolutePath('storybook-dark-mode'),
     getAbsolutePath('@storybook/addon-a11y'),
     getAbsolutePath('@storybook/addon-mdx-gfm'),
-    '@chromatic-com/storybook',
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-mdx-gfm'),
   ],
 
   framework: {
