39bedc8951ce372f60f1a964afa74e177971322b
Benoit Deziel <benoitdeziel@gmail.com>
2024-11-10 10:34:17 -0500

    build: fix storybook

--- packages/ark/vite.config.ts
+++ packages/ark/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  root: __dirname,
   cacheDir: '../../node_modules/.vite/packages/ark',
 
-  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  plugins: [react(), tsconfigPaths()],
 
   test: {
     cache: {
