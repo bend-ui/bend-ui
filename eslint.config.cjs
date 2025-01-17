eafea771c8868edee41c237336ea07017c8dd2fc
Benoit Deziel <benoitdeziel@gmail.com>
2025-01-17 16:47:06 -0500

    chore: update eslint config to exclude nx folder

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -5,7 +5,7 @@ module.exports = [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist', '**/styled-system', '**/.source'],
+    ignores: ['**/dist', '**/styled-system', '**/.source', '.nx'],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
