ffa397489461f3481536b817902f2325d35bdf37
Benoit Deziel <benoitdeziel@gmail.com>
2025-10-23 21:45:59 -0400

    chore: migrate nx to latest

--- apps/playground-panda-radix/next.config.js
+++ apps/playground-panda-radix/next.config.js
@@ -14,11 +14,7 @@ const nextConfig = {
       },
     ],
   },
-  nx: {
-    // Set this to true if you would like to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
+  nx: {},
 };
 
 const plugins = [
