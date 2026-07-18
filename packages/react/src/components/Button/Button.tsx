816c23c06763474cb8fa58acf199dd7643010e35
Benoit Deziel <benoitdeziel@gmail.com>
2026-07-18 08:28:31 -0400

    feat: create button utility

--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -1,61 +1,10 @@
 'use client';
 
-import { ReactNode } from 'react';
-import { button, ButtonVariantProps } from '@bend-ui/styled-system/recipes';
-import { HTMLBendUIProps, bend } from '../factory';
-import { Assign } from '@bend-ui/styled-system/types';
-import { withParts, withRecipe } from '../../utils';
+import {
+  createButton,
+  type ButtonProps as InternalButtonProps,
+} from '@bend-ui/internal';
 
-const Root = withRecipe(bend.button, button, 'root');
+export type ButtonProps = InternalButtonProps;
 
-const Icon = withParts(bend.div, 'icon');
-
-const Label = withParts(bend.span, 'label');
-
-export interface ButtonProps
-  extends Assign<HTMLBendUIProps<'button'>, ButtonVariantProps> {
-  /** Disable the button */
-  isDisabled?: boolean;
-  /** Set the button in a loading state */
-  isLoading?: boolean;
-  /** The icon to display at the start of the button */
-  icon?: ReactNode;
-  /** The icon to display at the end of the button */
-  iconEnd?: ReactNode;
-}
-
-const Component = (props: ButtonProps) => {
-  const {
-    className,
-    children,
-    icon,
-    iconEnd,
-    isDisabled,
-    isLoading,
-    isFull,
-    disabled,
-    ...rest
-  } = props;
-
-  return (
-    <Root
-      data-disabled={disabled || isDisabled}
-      aria-disabled={disabled || isDisabled}
-      aria-busy={isLoading}
-      disabled={disabled}
-      {...rest}
-    >
-      {icon && <Icon>{icon}</Icon>}
-      {children && <Label>{children}</Label>}
-      {iconEnd && <Icon>{iconEnd}</Icon>}
-    </Root>
-  );
-};
-
-Component.displayName = 'Button';
-
-export const Button = Object.assign(Component, {
-  Root,
-  Icon,
-  Label,
-});
+export const Button = createButton();
--- packages/react/src/factory.ts
+++ packages/react/src/factory.ts
@@ -1,2 +1,14 @@
-export * from './utils';
+export * from './utils/index';
 export * from './components/factory';
+export {
+  createButton,
+  createCompoundComponent,
+  type ButtonComponent,
+  type ButtonOwnProps,
+  type ButtonProps,
+  type CompoundComponent,
+  type CompoundSlotDefinition,
+  type CompoundSlotProps,
+  type CreateButtonOptions,
+  type CreateCompoundComponentOptions,
+} from '@bend-ui/internal';
